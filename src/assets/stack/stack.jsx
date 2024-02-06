d40c66dd2633017a6df235864f5b667e6c2f92b8
elvxk <pradiptaramadan9c@gmail.com>
2024-02-07 03:20:04 +0700

    Skill Assets 🏂

---
+++ src/assets/stack/stack.jsx
@@ -0,0 +1,40 @@
+import react from "./react.webp";
+import js from "./js.webp";
+import tailwind from "./tailwind.webp";
+import bs from "./bs.webp";
+import py from "./py.webp";
+import php from "./php.webp";
+import vite from "./vite.webp";
+import git from "./git.webp";
+import css from "./css.webp";
+import html from "./html.webp";
+import laravel from "./laravel.webp";
+import ci from "./ci.webp";
+import mysql from "./mysql.webp";
+import ps from "./ps.webp";
+import pr from "./pr.webp";
+import fl from "./fl.webp";
+import cb from "./cb.webp";
+import figma from "./figma.webp";
+
+const stack = [
+  vite,
+  git,
+  react,
+  tailwind,
+  bs,
+  php,
+  js,
+  py,
+  html,
+  css,
+  laravel,
+  ci,
+  mysql,
+  ps,
+  pr,
+  fl,
+  cb,
+  figma,
+];
+export default stack;
