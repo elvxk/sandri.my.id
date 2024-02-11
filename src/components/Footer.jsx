9033cc662e363a82af5338d9d315d88229c9fb55
elvxk <pradiptaramadan9c@gmail.com>
2024-02-11 21:47:44 +0700

    add Footer 🍕

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import Logo from "../components/Logo";
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center text-xl font-bold bg-cpink py-10">
-      <h1>FOOTER</h1>
+    <footer className="relative w-full overflow-hidden">
+      <div className="bg_parallax bg-fixed h-full w-full absolute opacity-[40%] dark:opacity-[7%]"></div>
+      <div className="flex flex-col py-4 container mx-auto justify-center items-center gap-2">
+        <Logo className="fill-cdark z-10 dark:fill-cwhite hover:fill-cpink dark:hover:fill-ccyan transition-all" />
+        <h1 className="font-squada z-10 text-sm lg:text-md text-cdark dark:text-cwhite flex flex-col justify-center items-center">
+          MADE WITH LOVE ❤ 2024 BY ELVXK
+          <p className="font-blinker text-cpink dark:text-ccyan">
+            All Right Reserved
+          </p>
+        </h1>
+      </div>
     </footer>
   );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -64,14 +64,21 @@ const Contact = () => {
     >
       <ToastContainer />
       <div className="flex flex-col items-center">
-        <h1 className="font-blinker text-ccyan text-4xl font-bold">
+        <h1
+          className="font-blinker text-ccyan text-4xl font-bold"
+          data-aos="fade-up"
+        >
           Contact Me
         </h1>
-        <p className="font-blinker text-cdark dark:text-cwhite text-center justify-center items-center my-2 text-lg">
+        <p
+          data-aos="fade-up"
+          className="font-blinker text-cdark dark:text-cwhite text-center justify-center items-center my-2 text-lg"
+        >
           send me a message via the email form below
         </p>
 
         <form
+          data-aos="fade-up"
           ref={form}
           onSubmit={sendEmail}
           className="flex w-full flex-col font-blinker text-cdark dark:text-cwhite bg-cdark/25 p-4 dark:bg-cwhite/10 max-w-screen-sm mx-auto"
@@ -107,13 +114,22 @@ const Contact = () => {
             />
           </div>
         </form>
-        <h1 className="font-blinker font-bold text-2xl text-ccyan mt-14 lg:mt-32">
+        <h1
+          className="font-blinker font-bold text-2xl text-ccyan mt-14 lg:mt-32"
+          data-aos="fade-up"
+        >
           Social
         </h1>
-        <p className="font-blinker text-cdark dark:text-cwhite text-center justify-center items-center my-2 mb-6 text-lg">
+        <p
+          data-aos="fade-up"
+          className="font-blinker text-cdark dark:text-cwhite text-center justify-center items-center my-2 mb-6 text-lg"
+        >
           find me on the following social media
         </p>
-        <div className="flex flex-wrap gap-6 justify-center items-center text-2xl text-cdark dark:text-cwhite">
+        <div
+          data-aos="zoom-in"
+          className="flex flex-wrap gap-6 justify-center items-center text-2xl text-cdark dark:text-cwhite"
+        >
           <a
             href="https://instagram.com/elvxk"
             target="_blank"
